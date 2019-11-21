24060d36b73e01073b109e6eb58371e89d8e327c
Mike <n@n.n>
2019-11-21 21:00:52 +0200

    c

--- dataset/get_data_wget.sh
+++ dataset/get_data_wget.sh
@@ -4,8 +4,9 @@ do
 	echo Getting $V
 	mkdir $V
 	cd $V
-	wget http://images.cocodataset.org/zips/$V.zip
-	unzip $V.zip
+	wget --timeout 10 http://images.cocodataset.org/zips/$V.zip
+	unzip -q $V.zip
+	rm $V.zip
 	cd ..
 done
 
@@ -13,6 +14,7 @@ mkdir /annotations
 cd /annotations
 wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip 
 unzip annotations_trainval2017.zip
+rm annotations_trainval2017.zip
 cd ..
 
 echo Done
