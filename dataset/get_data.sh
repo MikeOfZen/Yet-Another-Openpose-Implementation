aa6b95ae7c157bed0e862015e0698f9fd6c397be
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-11-21 18:01:17 +0000

    c

--- dataset/get_data.sh
+++ dataset/get_data.sh
@@ -5,11 +5,11 @@ mkdir test2017
 
 
 echo getting train2017
-gsutil -m rsync gs://images.cocodataset.org/train2017 /train2017/
+gsutil -m rsync gs://images.cocodataset.org/train2017 ./train2017/
 echo getting val2017
-gsutil -m rsync gs://images.cocodataset.org/val2017 /val2017/
+gsutil -m rsync gs://images.cocodataset.org/val2017 ./val2017/
 echo getting test2017
-gsutil -m rsync gs://images.cocodataset.org/test2017 /test2017/
+gsutil -m rsync gs://images.cocodataset.org/test2017 ./test2017/
 
 mkdir /annotations
 cd /annotations
