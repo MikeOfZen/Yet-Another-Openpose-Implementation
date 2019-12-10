5b32b6e64e778d8ecac121259da03264a0784319
Mike <n@n.n>
2019-12-11 00:34:12 +0200

    organize

--- models/six_stage_linear_model.py
+++ models/six_stage_linear_model.py
@@ -1,22 +1,29 @@
 import tensorflow as tf
-from default_config import IMAGE_HEIGHT,IMAGE_WIDTH,PAF_NUM_FILTERS,HEATMAP_NUM_FILTERS,BATCH_NORMALIZATION_ON,INCLUDE_MASK,LABEL_HEIGHT,LABEL_WIDTH
 
 
-INPUT_SHAPE=(IMAGE_HEIGHT, IMAGE_WIDTH, 3)
-MASK_SHAPE=(LABEL_HEIGHT, LABEL_WIDTH, 1)
-
-class ModelMaker():
+class ModelMaker:
     """Creates a model for the OpenPose project, structre is 10 layers of VGG16 followed by a few convultions, and 6 stages 
     of (PAF,PAF,PAF,PAF,kpts,kpts) also potentially includes a mask stacked with the outputs"""
     
-    def __init__(self):
+    def __init__(self,config):
+        self.IMAGE_HEIGHT=config.IMAGE_HEIGHT
+        self.IMAGE_WIDTH=config.IMAGE_WIDTH
+        self.PAF_NUM_FILTERS=config.PAF_NUM_FILTERS
+        self.HEATMAP_NUM_FILTERS=config.HEATMAP_NUM_FILTERS
+        self.BATCH_NORMALIZATION_ON=config.BATCH_NORMALIZATION_ON
+        self.INCLUDE_MASK=config.INCLUDE_MASK
+        self.LABEL_HEIGHT=config.LABEL_HEIGHT
+        self.LABEL_WIDTH=config.LABEL_WIDTH
+        self.INPUT_SHAPE = (config.IMAGE_HEIGHT, config.IMAGE_WIDTH, 3)
+        self.MASK_SHAPE = (config.LABEL_HEIGHT, config.LABEL_WIDTH, 1)
+
         self.conv_block_nfilters = 96
         self.stage_final_nfilters = 512
 
         self._get_vgg_layer_config_weights()
 
     def _get_vgg_layer_config_weights(self):
-        vgg_input_model = tf.keras.applications.VGG16(weights='imagenet', include_top=False, input_shape=INPUT_SHAPE)
+        vgg_input_model = tf.keras.applications.VGG16(weights='imagenet', include_top=False, input_shape=self.INPUT_SHAPE)
         name_last_layer = "block3_pool"
         
         self.vgg_layers=[]
@@ -47,11 +54,11 @@ class ModelMaker():
         return x
 
     def _make_conv_block(self, x, conv_block_filters, name):
-        if BATCH_NORMALIZATION_ON: x = tf.keras.layers.BatchNormalization(name=name + "_bn3")(x)
+        if self.BATCH_NORMALIZATION_ON: x = tf.keras.layers.BatchNormalization(name=name + "_bn3")(x)
         x1 = tf.keras.layers.Conv2D(conv_block_filters, 3, padding="same", activation='relu', name=name + "_conv1")(x)
-        if BATCH_NORMALIZATION_ON: x1 = tf.keras.layers.BatchNormalization(name=name + "_bn1")(x1)
+        if self.BATCH_NORMALIZATION_ON: x1 = tf.keras.layers.BatchNormalization(name=name + "_bn1")(x1)
         x2 = tf.keras.layers.Conv2D(conv_block_filters, 3, padding="same", activation='relu', name=name + "_conv2")(x1)
-        if BATCH_NORMALIZATION_ON: x2 = tf.keras.layers.BatchNormalization(name=name + "_bn2")(x2)
+        if self.BATCH_NORMALIZATION_ON: x2 = tf.keras.layers.BatchNormalization(name=name + "_bn2")(x2)
         x3 = tf.keras.layers.Conv2D(conv_block_filters, 3, padding="same", activation='relu', name=name + "_conv3")(x2)
 
         output = tf.keras.layers.concatenate([x1, x2, x3], name=name + "_output")
@@ -69,7 +76,7 @@ class ModelMaker():
         x = self._make_conv_block(x, conv_block_filters, name + "_block5")
 
         x = tf.keras.layers.Conv2D(self.stage_final_nfilters, 1, padding="same", activation='relu', name=name + "_final1conv")(x)
-        if BATCH_NORMALIZATION_ON: x = tf.keras.layers.BatchNormalization(name=name + "_finalbn1")(x)
+        if self.BATCH_NORMALIZATION_ON: x = tf.keras.layers.BatchNormalization(name=name + "_finalbn1")(x)
         x = tf.keras.layers.Conv2D(outputs, 1, padding="same", activation=last_activation, name=name + "_preoutput")(x)
         return x
 
@@ -93,7 +100,7 @@ class ModelMaker():
         return new_outputs
 
     def create_models(self):        
-        input_tensor = tf.keras.layers.Input(shape=INPUT_SHAPE) #first layer of the model
+        input_tensor = tf.keras.layers.Input(shape=self.INPUT_SHAPE) #first layer of the model
 
         #mask_string="_pre_mask" if INCLUDE_MASK else ""
 
@@ -103,24 +110,24 @@ class ModelMaker():
         stage0_output = self._make_stage0(stage00_output)
         # PAF stages
         # stage 1
-        stage1_output = self._make_stageI([stage0_output], "s1pafs", 96, PAF_NUM_FILTERS,"linear")
+        stage1_output = self._make_stageI([stage0_output], "s1pafs", 96, self.PAF_NUM_FILTERS,"linear")
         # stage 2
-        stage2_output = self._make_stageI([stage1_output, stage0_output], "s2pafs", 128, PAF_NUM_FILTERS,"linear")
+        stage2_output = self._make_stageI([stage1_output, stage0_output], "s2pafs", 128, self.PAF_NUM_FILTERS,"linear")
         # stage 3
-        stage3_output = self._make_stageI([stage2_output, stage0_output], "s3pafs", 128, PAF_NUM_FILTERS,"linear")
+        stage3_output = self._make_stageI([stage2_output, stage0_output], "s3pafs", 128, self.PAF_NUM_FILTERS,"linear")
         # stage 4
-        stage4_output = self._make_stageI([stage3_output, stage0_output], "s4pafs", 128, PAF_NUM_FILTERS,"linear")
+        stage4_output = self._make_stageI([stage3_output, stage0_output], "s4pafs", 128, self.PAF_NUM_FILTERS,"linear")
         # keypoint heatmap stages
         # stage5
-        stage5_output = self._make_stageI([stage4_output, stage0_output], "s5kpts", 96, HEATMAP_NUM_FILTERS,"relu")
+        stage5_output = self._make_stageI([stage4_output, stage0_output], "s5kpts", 96, self.HEATMAP_NUM_FILTERS,"relu")
         # stage6
-        stage6_output = self._make_stageI([stage5_output, stage4_output, stage0_output], "s6kpts", 128, HEATMAP_NUM_FILTERS,"relu")
+        stage6_output = self._make_stageI([stage5_output, stage4_output, stage0_output], "s6kpts", 128, self.HEATMAP_NUM_FILTERS,"relu")
 
         training_inputs=input_tensor
         training_outputs = [stage1_output, stage2_output, stage3_output, stage4_output, stage5_output, stage6_output]
 
-        if INCLUDE_MASK:  #this is used to pass the mask directly to the loss function through the model
-            mask_input= tf.keras.layers.Input(shape=MASK_SHAPE)
+        if self.INCLUDE_MASK:  #this is used to pass the mask directly to the loss function through the model
+            mask_input= tf.keras.layers.Input(shape=self.MASK_SHAPE)
             training_outputs=self._psd_zero_mask_to_outputs(training_outputs,mask_input)
             training_inputs=(input_tensor,mask_input)
 
@@ -133,17 +140,22 @@ class ModelMaker():
 
         return train_model,test_model
 
-@tf.function
-def place_training_labels(elem):
-    """Distributes labels into the correct configuration for the model, ie 4 PAF stage, 2 kpt stages
-    must match the model"""
-    paf_tr = elem['paf']
-    kpt_tr = elem['kpts']
-    image = elem['image']
 
-    if INCLUDE_MASK:
-        inputs = (image,elem['mask'])
-    else:
-        inputs = image
+class ModelDatasetComponent:
+    def __init__(self,config):
+        self.INCLUDE_MASK=config.INCLUDE_MASK
 
-    return inputs, (paf_tr, paf_tr, paf_tr, paf_tr, kpt_tr, kpt_tr)  # this should match the model outputs, and is different for each model
+    @tf.function
+    def place_training_labels(self,elem):
+        """Distributes labels into the correct configuration for the model, ie 4 PAF stage, 2 kpt stages
+        must match the model"""
+        paf_tr = elem['paf']
+        kpt_tr = elem['kpts']
+        image = elem['image']
+
+        if self.INCLUDE_MASK:
+            inputs = (image,elem['mask'])
+        else:
+            inputs = image
+        return inputs, (paf_tr, paf_tr, paf_tr, paf_tr, kpt_tr, kpt_tr)  # this should match the model outputs, and is different for each model
+
