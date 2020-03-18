eb916b826fee6eaef191a690b0980fbef948ba92
Mike <n@n.n>
2020-03-18 04:44:57 +0200

    fix input mismatch

--- applications/model_wrapper.py
+++ applications/model_wrapper.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import post_processing.post as post
 import configs.post_config as post_config
 import configs.keypoints_config as kpts_config
+import configs.default_config as def_config
 
 post.Skeletonizer.config(kpts_config.KEYPOINTS_DEF, kpts_config.JOINTS_DEF, post_config)
 post.Skeleton.config(kpts_config.KEYPOINTS_DEF, kpts_config.JOINTS_DEF)
@@ -12,7 +13,7 @@ class ModelWrapper:
         self.model = tf.keras.models.load_model(model_path)
 
     def process_image(self, img):
-        input_img = tf.image.resize(img, (360, 360))
+        input_img = tf.image.resize(img, (def_config.IMAGE_HEIGHT, def_config.IMAGE_WIDTH))
         input_img = tf.image.convert_image_dtype(input_img, dtype=tf.float32)
         input_img /= 255
         input_img = input_img[tf.newaxis, ...]
